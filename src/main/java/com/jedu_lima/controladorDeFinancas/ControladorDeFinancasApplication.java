a5af7de691e8ac9183f9f3525f5359c7d79cfc1c
José Eduardo <limaedu18@hotmail.com>
2024-11-01 15:39:38 -0300

    first commit

---
+++ src/main/java/com/jedu_lima/controladorDeFinancas/ControladorDeFinancasApplication.java
@@ -0,0 +1,13 @@
+package com.jedu_lima.controladorDeFinancas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControladorDeFinancasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControladorDeFinancasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jedu_lima/controladorDeFinancas/ControladorDeFinancasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jedu_lima.controladorDeFinancas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControladorDeFinancasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
